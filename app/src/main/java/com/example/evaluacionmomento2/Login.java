6266c8aecea4dee95de45c7612fa9271cde9501e
Wilmer Agudelo <wilmer.com.co@gmail.com>
2024-10-19 11:28:27 -0500

    first commit

---
+++ app/src/main/java/com/example/evaluacionmomento2/Login.java
@@ -0,0 +1,39 @@
+package com.example.evaluacionmomento2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    private Button btnIniciarSesion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnIniciarSesion = findViewById(R.id.btnIniciarSasion);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Product.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
